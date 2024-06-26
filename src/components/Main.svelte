398bebb45825b3609e73d7d9623a6a220c8e8fa9
David Qvarnström <davidqvarnstrom@gmail.com>
2024-06-26 11:54:38 +0200

    added projects & start on about

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -20,6 +20,31 @@
             description:
                 "Test for model robustness with customized test profiles and receive exhaustive reporting on the performance and potential vulnerabilities of your model.",
         },
+        {
+            name: "Event Stream Monitor",
+            icon: "fa-solid fa-pie-chart",
+            description:
+                "Test for model robustness with customized test profiles and receive exhaustive reporting on the performance and potential vulnerabilities of your model.",
+        },
+    ];
+
+    let benefits = [
+        {
+            metric: "10x",
+            name: "a self taught developer",
+            description:
+                "I taught myself to code using free online resources and absolutely fell in love with the creativity and problem solving that is involved in developing and engineering innovative new online experiences. Starting of with JavaScript, HTML & CSS and evolving my knowledge base to include JavaScript frameworks, backend programming, design, cloud services and much much more.",
+        },
+        {
+            name: "a product design & UX finatic",
+            description:
+                "Carefully crafting and designing amazing user experiences allows me to express and experiment with every morsel of creativity I have. I love the challenge of learning new design concepts and enabling users with amazing online experiences.",
+        },
+        {
+            name: "an excellent communicator",
+            description:
+                "Communication is key and it's a paramount value of mine. I believe in transparency and constructive communication above all else. This helps me develop deep relationships and ensures my effectiveness and productivity in any work space with any team.",
+        },
     ];
 </script>
 
@@ -40,6 +65,7 @@
                 My <span class="text-violet-400">favorite tech</span> includes Java
                 Javascript Python Node.js Sveltekit CSS C C++ PostgreSQL ClickHouse
             </p>
+
             <button
                 class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
             >
@@ -67,24 +93,102 @@
             </h3>
         </div>
 
-        <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
-            <Step step={steps[0]}>
-                <p>
-                    A Project in AOOP(Advanced Object Oriented Programming)
-                    course. The game is developed in Java and is a tile based
-                    puzzle game. We managed to cranck in 5 desginpatterns <strong
-                        class="text-violet-400"
-                        >MVC, Observer, Strategy, Prototype and Memento Pattern</strong
-                    > Overengineering at its finsets, this gave us a solid 5! and
-                    "One of the best reports" from the course.
-                </p>
-            </Step>
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-10">
+            <a href="https://github.com/davidQva/aoop-project" target="_blank">
+                <Step step={steps[0]}>
+                    <p>
+                        A Project in AOOP(Advanced Object Oriented Programming)
+                        course. The game is developed in Java and is a tile
+                        based puzzle game. We managed to cranck in 5
+                        desginpatterns <strong class="text-violet-400"
+                            >MVC, Observer, Strategy, Prototype and Memento
+                            Pattern</strong
+                        > Overengineering at its finsets, this gave us a solid 5!
+                        and "One of the best reports" from the course.
+                    </p>
+                </Step>
+            </a>
             <Step step={steps[1]}>
-                <p></p></Step
+                <p>
+                    Using a camera and a robot arm connected to a Raspberry Pi
+                    to sort cubes from a conveyor belt. Leveraging <strong
+                        class="text-violet-400"
+                        >Python, OpenCV and Dobot
+                    </strong>, we create an integrated system capable of taking
+                    orders from a website and automatically collecting the cubes
+                    from the belt.
+                </p></Step
             >
             <Step step={steps[2]}>
-                <p></p>
+                <p>
+                    A system to monitor and control greenhouse temperature.
+                    Created with IAR IDE and coded in C, the software logs
+                    temperature every minute, manages light exposure, and
+                    provides real-time alerts. It uses linked lists for data
+                    management and includes a fast mode for testing. The LCD
+                    user interface, navigated via a numeric keypad, allows users
+                    to configure settings and view logged data.
+                </p>
             </Step>
+            <Step step={steps[3]}>
+                <p>
+                    "Bachelor's thesis project involves a module for streaming
+                    data from an event-driven system to a ClickHouse database.
+                    It's tailored for integration with the SvelteKit framework,
+                    featuring a Node.js backend and a SvelteKit frontend.
+                    <strong> Currently under development. </strong>"
+                </p>
+            </Step>
+        </div>
+    </section>
+    <section
+        id="about"
+        class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
+    >
+        <div
+            class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+        >
+            <h6 class="text-large sm:text-xl md:text-2xl">
+                Want to know more?
+            </h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                A bit <span class="poppins text-violet-400">about</span> me.
+            </h3>
+        </div>
+        <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">
+            I am . . .
+        </p>
+        <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+            {#each benefits as benefit, index}
+                <!-- <div class="flex flex-col gap-2 mx-auto">
+                    <div class="flex items-end gap-4">
+                        <p
+                            class="poppins text-6xl sm:text-7xl md:text-8xl text-slate-500 font-medium"
+                        >
+                            {benefit.metric}
+                        </p>
+                        <p
+                            class="text-xl sm:text-2xl md:text-3xl capitalize pb-2"
+                        >
+                            {benefit.name}
+                        </p>
+                    </div>
+                    <p class="text-center italic">- {benefit.description}</p>
+                </div> -->
+                <div class="flex gap-6 sm:gap-8">
+                    <p
+                        class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold"
+                    >
+                        0{index + 1}
+                    </p>
+                    <div class="flex flex-col gap-6 sm:gap-8">
+                        <h3 class="text-2xl sm:text-3xl md:text-5xl">
+                            {benefit.name}
+                        </h3>
+                        <p>{benefit.description}</p>
+                    </div>
+                </div>
+            {/each}
         </div>
     </section>
 </main>
