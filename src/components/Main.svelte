eaf45e82d9794ac9cce92ac66773fccdb08edae6
David Qvarnström <davidqvarnstrom@gmail.com>
2025-04-27 10:43:18 +0200

    yes?

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -163,7 +163,7 @@
             class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
         >
             <h6 class="text-large sm:text-xl md:text-2xl">
-                Want to know more?
+                Want to know more, YES?
             </h6>
             <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
                 A breif overview <span class="poppins text-violet-400"
